02b0242b821d3c7a501e41553dc241b93c19748f
Ivan Dekhtiarov <ivan.dekhtiarov@gmail.com>
2021-01-06 09:24:24 +0200

    Initial Commit

---
+++ NBUCurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NBUCurrencyConverter
+//
+//  Created by Ivan Dekhtiarov on 06.01.2021.
+//  Copyright © 2021 Ivan Dekhtiarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
