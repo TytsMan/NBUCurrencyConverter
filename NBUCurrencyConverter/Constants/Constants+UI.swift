013eedf4e184afc1386b0ccbe9292f3b656e5edd
Ivan Dekhtiarov <ivan.dekhtiarov@gmail.com>
2021-01-11 02:31:48 +0200

    add  ui constants

--- NBUCurrencyConverter/Constants/Constants+UI.swift
+++ NBUCurrencyConverter/Constants/Constants+UI.swift
@@ -15,8 +15,13 @@ extension Constants {
 
 extension Constants.UI {
     
-    static let safeAreaPortrait = UIEdgeInsets(top: 20, left: 15, bottom: 20, right: 15)
-    static let insets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    /// Dimensions
+    static let kSafeAreaPortrait = UIEdgeInsets(top: 20, left: 15, bottom: 20, right: 15)
+    static let kInsets = UIEdgeInsets(top: 4, left: 4, bottom: 8, right: 8)
+    
+    static let kElementHeight: CGFloat = 34
+    static let kTextViewWidth: CGFloat = 150
+    static let kPickerViewHeight: CGFloat = 150
 }
 
 extension Constants.UI {
@@ -24,19 +29,16 @@ extension Constants.UI {
     enum CurrencyList {
         
         static let kTitle = "Currencies"
-        static let kBackgroundColor: UIColor = .systemBackground
+        static let kBackgroundColor: UIColor = .white
         
         enum CurrencyListTableView {
-            
-            static let kBackgroundColor: UIColor = .systemBackground
-            
+            static let kBackgroundColor: UIColor = .white
          }
         
         enum ExchangeButton{
             static let kTitle = "Exchange"
             static let kTitleColor: UIColor = .white
             static let kBackgroundColor: UIColor = .systemBlue
-            static let height: CGFloat = 34
         }
         
         enum CurrencyCell {
@@ -44,4 +46,22 @@ extension Constants.UI {
         }
         
     }
+}
+
+extension Constants.UI {
+    
+    enum Converter {
+        static let kTitle = "Converter"
+        static let kBackgroundColor: UIColor = .white
+        static let kOriginCurrency = Currency(name: "UAH", description: "gruvnya", rate: 1.0, date: Date())
+        
+        static let kFirstTextViewTag = 1000
+        static let kSecondTextViewTag = 1001
+        
+        static let kFirstPickerViewTag = 1002
+        static let kSecondPickerViewTag = 1003
+
+        
+    }
+    
 }
